48f4a04b64d75dcbd32eb5fb69b9e950736bd318
Aditya Dey <deyaditya2004@gmail.com>
2025-04-12 00:21:51 +0530

    Add Middlewares

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// external imports
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+// internal imports
+const{notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler");
+
 
 const app = express();
 dotenv.config();
@@ -14,3 +21,28 @@ mongoose.connect(process.env.MONGO_CNNECTION_STRING, {
 .catch(err => console.log(err));
 
 // request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routig setup
+
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);  
+});
