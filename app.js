80c069b8be323d19c3275eca2c735608da82090a
Aditya Dey <deyaditya2004@gmail.com>
2025-04-16 06:49:48 +0530

    UI READY

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
 
 // internal imports
-const{notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler");
-
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
 dotenv.config();
 
-// Database Connection
-mongoose.connect(process.env.MONGO_CNNECTION_STRING, {
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log("Database Connection Successfull!"))
-.catch(err => console.log(err));
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch((err) => console.log(err));
 
 // request parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
@@ -33,8 +39,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// routig setup
-
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
 // 404 not found handler
 app.use(notFoundHandler);
@@ -42,7 +50,6 @@ app.use(notFoundHandler);
 // common error handler
 app.use(errorHandler);
 
-
 app.listen(process.env.PORT, () => {
-  console.log(`app listening to port ${process.env.PORT}`);  
+  console.log(`app listening to port ${process.env.PORT}`);
 });
